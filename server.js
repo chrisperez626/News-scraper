7ca026d309ff6d93dd66de8a2f6d531740931120
chrisperez626 <chrisperez626@gmail.com>
2018-05-04 13:50:19 -0400

    initial program files

---
+++ server.js
@@ -0,0 +1,5 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var cheerio = require('cheerio');
+var request = require('request');
